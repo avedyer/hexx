2508473455779301b5a35ac2645eb7eb300b1092
avedyer <avedyer@gmail.com>
2022-07-12 12:47:41 -0400

    adjusted pixel data functions to account for positioning of objects. buggy around borders for unknown reason(s)

--- src/wheel.js
+++ src/wheel.js
@@ -9,20 +9,24 @@ function Wheel(props) {
   const [tracking, setTracking] = useState(false)
   const [handleCoords, setHandleCoords] = useState()
 
-  const wheelEl = 'html-wheel';
+  const wheelEl = 'wheel';
+  const containerSize = 384
+  const radius = 320
 
   useEffect(() => {
+    //Adjust droppers when settings are changed outside of handle movement
     if (handleCoords) {
       rotateDroppers(handleCoords)
     }
   }, [props.quantity, props.range, lightness])
 
+
   useEffect(() => {
+    //Sets default handle coordinates for proper rendering of new droppers.
     if (!handleCoords) {
       const wheelNode = document.getElementById(wheelEl);
       if (wheelNode) {
-        const size = wheelNode.offsetWidth / 2;
-        const x = size, y = 0
+        const x = radius, y = 0
         setHandleCoords(boundCoordinates({x: x, y: y}))
       }
     }
@@ -37,24 +41,25 @@ function Wheel(props) {
   }
 
   function getPixelColor(coordArray, node) {
-    //Sets RGB state value to color of a pixel at event location.
+
+    //Sets RGB state value based on array of coordinates
+
     domtoimage.toPixelData(node)
       .then((pixels) => {
+
         let rect = node.getBoundingClientRect();
 
-        
         let newArray = []
 
         coordArray.forEach((coords) => {
 
-          //pixel data was black at right boundary of color circle. this patches it. don't know why.
-
-          let adjX = coords.x + rect.x
-          if (adjX >= node.scrollWidth) {
-            adjX = node.scrollWidth - 1
+          coords.x -= radius;
+          
+          if (coords.x > 241) {
+            coords.x = 241
           }
 
-          const pixelAtXYOffset = (4 * (coords.y + rect.y) * node.scrollHeight) + (4 * adjX);
+          const pixelAtXYOffset = (4 * (coords.y + rect.y) * node.scrollHeight) + (4 * (coords.x + rect.x));
           const pixelData = pixels.slice(pixelAtXYOffset, pixelAtXYOffset + 3);
 
           const newRGB = `rgb(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]})`
@@ -69,6 +74,8 @@ function Wheel(props) {
 
   function moveHandle(e) {
 
+    //Move primary handle dropper based on event location.
+
     if (!tracking && e.type !== 'click') {
       return
     }
@@ -79,7 +86,7 @@ function Wheel(props) {
     coords = boundCoordinates(coords)
 
     const handle = document.getElementById('handle')
-    handle.style.top = `${coords.y - 10}px` //10 px adjustment centers mouse within the dropper
+    handle.style.top = `${coords.y - 10}px` //10px adjustment centers mouse within the dropper
     handle.style.left = `${coords.x - 10}px`
 
     if (props.quantity > 1) {
@@ -87,7 +94,7 @@ function Wheel(props) {
     }
 
     else {
-      getPixelColor([coords], wheelNode)
+      getPixelColor([{x: coords.x - 10, y: coords.y - 10}], wheelNode)
     }
 
     setHandleCoords(coords)
@@ -96,41 +103,44 @@ function Wheel(props) {
   function rotateDroppers(coords) {
 
     const wheelNode = document.getElementById(wheelEl)
-
-    const size = wheelNode.offsetWidth / 2
-    const sides =  {x: coords.x - size, y: -coords.y + size};
+    const sides =  {x: coords.x - radius, y: -coords.y + radius};
     const hypotenuse = Math.sqrt((sides.x**2) + (sides.y**2));
 
-    const maxRange = props.quantity == 2 ? 1 : 1 / props.quantity
+    const maxRange = props.quantity == 2 ? 1 : 1 / props.quantity  //Maximum fraction of the circle around which the dropper can rotate. One dropper can rotate freely.
 
-    let radAdjustment
+    let handleRotation  //Degree of rotation of handle. Applied as adjustment to each dropper.
 
     if (sides.x > 0) {
       if (sides.y >= 0) {
-        radAdjustment = Math.atan(sides.x / sides.y)
+        handleRotation = Math.atan(sides.x / sides.y)
       }
       else {
-        radAdjustment = Math.atan(sides.x / sides.y) + Math.PI
+        handleRotation = Math.atan(sides.x / sides.y) + Math.PI
       }
     }
     else {
       if (sides.y >= 0) {
-        radAdjustment = Math.atan(sides.x / sides.y) + 2 * Math.PI
+        handleRotation = Math.atan(sides.x / sides.y) + 2 * Math.PI
       }
       else {
-        radAdjustment = Math.atan(sides.x / sides.y) + Math.PI
+        handleRotation = Math.atan(sides.x / sides.y) + Math.PI
       }
     }
 
-    const radAngle = Math.PI * 2 * (props.range * maxRange)
+    const dropperRotation = Math.PI * 2 * (props.range * maxRange) //Baseline rotation for each dropper.
 
     let coordArray = [coords]
 
     document.querySelectorAll('.eyedropper').forEach((eyedropper, index) => {
       const step = props.quantity - index - 1
-      const ratio = {x: Math.sin((radAngle * step) + radAdjustment), y: Math.cos((radAngle  * step) + radAdjustment)}
-      let newCoords = ({x: Math.floor((hypotenuse * ratio.x) + size), y: Math.floor(Math.abs((hypotenuse * ratio.y) - size))})
-      coordArray.push(newCoords)
+
+      //Ratio based on angle of rotation, multiplied with hypotenuse to find x and y coordincates 
+      const ratio = {x: Math.sin((dropperRotation * step) + handleRotation), y: Math.cos((dropperRotation  * step) + handleRotation)} 
+
+      //Rouding coordinate value is necessary for finding pixel color.
+      let newCoords = ({x: Math.floor((hypotenuse * ratio.x) + radius), y: Math.floor(Math.abs((hypotenuse * ratio.y) - radius))})
+      coordArray = insert(coordArray, 1, {x: newCoords.x - 10, y: newCoords.y - 10})
+
       eyedropper.style.left = `${newCoords.x - 10}px`
       eyedropper.style.top = `${newCoords.y - 10}px`
     })
@@ -140,20 +150,20 @@ function Wheel(props) {
 
   function boundCoordinates(coords) {
 
-    const wheelNode = document.getElementById(wheelEl)
-    const size = wheelNode.offsetWidth / 2
-    const sides =  {x: coords.x - size, y: -coords.y + size};
+    //Adjusts coordinate values to account for size of dropper element, and ensure it is not rendered outside bounds of wheel.
+
+    const sides =  {x: coords.x - radius, y: -coords.y + radius};
     const hypotenuse = Math.sqrt((sides.x**2) + (sides.y**2));
 
-    if (hypotenuse > size) {
+    if (hypotenuse > radius) {
       return
     }
 
-    const margin = hypotenuse + 21 - size // overspill from edge of color wheel, accounting for size of dropper
+    const margin = hypotenuse + 21 - radius //Overspill from edge of color wheel. Dropper radius of 10 is implied but is actually defined in CSS.
 
     if (margin > 0) {
 
-      //Gradually adjust position of dropper to avoid spilling over edge
+      //Gradually adjust position of dropper
 
       const radAngle = Math.abs(Math.atan(sides.x / sides.y))
       const ratio = radAngle * 2 / Math.PI
@@ -168,19 +178,28 @@ function Wheel(props) {
     return coords
   }
 
+  const insert = (arr, index, ...newItems) => [
+    // part of the array before the specified index
+    ...arr.slice(0, index),
+    // inserted items
+    ...newItems,
+    // part of the array after the specified index
+    ...arr.slice(index)
+  ]
+
   return (
-    <div>
-      <div id="wheel-container" onMouseLeave={() => setTracking(false)}>
+    <div id='wheel-app'>
+      <div id="wheel-container" style={{width: `${containerSize * 2}px`, height: `${containerSize * 2}px`}} onMouseLeave={() => setTracking(false)}>
         <div id={wheelEl}>
-          <canvas id="wheel" />
-          <canvas id="filter" style={{
+          <canvas id="hue" />
+          <canvas id="shade" style={{
             backgroundImage: `radial-gradient(hsl(0, 0%, ${lightness}) 0%, transparent 70%)`,
           }}/>
         </div>
         <div id="color-selector" 
           onMouseDown={() => setTracking(true)} 
             onMouseMove={(e) => moveHandle(e)} 
-            onMouseUp={() => setTracking(false)}
+            onMouseUp={() => setTracking(false)}  //Tracking state keeps handle from being sticky.
             onClick={(e) => moveHandle(e)}
           >
           <div id="handle" style={{top: '0', left: 'calc(50% - 10px)'}}/>
@@ -194,7 +213,7 @@ function Wheel(props) {
           <div className="color-display" key={`color${index}`} id={`color${index}`} style={{width: "48px", height: "48px", backgroundColor: rgb[index]}} />
         )}
       </div>
-      <div className="slidecontainer">
+      <div className="slidecontainer" style={{width: `${radius * 2}px`}}>
         <input type="range" 
           defaultValue="100" min="1" max="100" className="slider" id="slider" 
           onChange={(e) => setLightness(`${e.target.value}%`)} //Controls lightness setting of filter
